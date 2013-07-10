cf7288df2b8520fb70b6d13636464f9710e53d0c
Zach Taylor <zach.taylor673@gmail.com>
2013-07-09 22:43:00 -0500

    Stat removed
    PokemonNatures now use String stat

--- src/main/java/org/jpokemon/pmapi/pokemon/nature/ClassicNatures.java
+++ src/main/java/org/jpokemon/pmapi/pokemon/nature/ClassicNatures.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Random;
 
-import org.jpokemon.pmapi.util.Stat;
 import org.jpokemon.pmapi.util.Taste;
 
 /**
@@ -25,114 +24,114 @@ public final class ClassicNatures {
 	// Neutral Natures:
 
 	/** Provides the neutral nature `Bashful`. */
-	public static final PokemonNature BASHFUL = new PokemonNature().setName("Bashful").setStatIncreased(Stat.NONE)
-			.setStatDecreased(Stat.NONE).setTasteFavorite(Taste.NONE).setTasteDisliked(Taste.NONE);
+	public static final PokemonNature BASHFUL = new PokemonNature().setName("Bashful").setStatIncreased("NONE")
+			.setStatDecreased("NONE").setTasteFavorite(Taste.NONE).setTasteDisliked(Taste.NONE);
 
 	/** Provides the neutral nature `Docile`. */
-	public static final PokemonNature DOCILE = new PokemonNature().setName("Docile").setStatIncreased(Stat.NONE)
-			.setStatDecreased(Stat.NONE).setTasteFavorite(Taste.NONE).setTasteDisliked(Taste.NONE);
+	public static final PokemonNature DOCILE = new PokemonNature().setName("Docile").setStatIncreased("NONE")
+			.setStatDecreased("NONE").setTasteFavorite(Taste.NONE).setTasteDisliked(Taste.NONE);
 
 	/** Provides the neutral nature `Hardy`. */
-	public static final PokemonNature HARDY = new PokemonNature().setName("Hardy").setStatIncreased(Stat.NONE)
-			.setStatDecreased(Stat.NONE).setTasteFavorite(Taste.NONE).setTasteDisliked(Taste.NONE);
+	public static final PokemonNature HARDY = new PokemonNature().setName("Hardy").setStatIncreased("NONE")
+			.setStatDecreased("NONE").setTasteFavorite(Taste.NONE).setTasteDisliked(Taste.NONE);
 
 	/** Provides the neutral nature `Quirky`. */
-	public static final PokemonNature QUIRKY = new PokemonNature().setName("Quirky").setStatIncreased(Stat.NONE)
-			.setStatDecreased(Stat.NONE).setTasteFavorite(Taste.NONE).setTasteDisliked(Taste.NONE);
+	public static final PokemonNature QUIRKY = new PokemonNature().setName("Quirky").setStatIncreased("NONE")
+			.setStatDecreased("NONE").setTasteFavorite(Taste.NONE).setTasteDisliked(Taste.NONE);
 
 	/** Provides the neutral nature `Serious`. */
-	public static final PokemonNature SERIOUS = new PokemonNature().setName("Serious").setStatIncreased(Stat.NONE)
-			.setStatDecreased(Stat.NONE).setTasteFavorite(Taste.NONE).setTasteDisliked(Taste.NONE);
+	public static final PokemonNature SERIOUS = new PokemonNature().setName("Serious").setStatIncreased("NONE")
+			.setStatDecreased("NONE").setTasteFavorite(Taste.NONE).setTasteDisliked(Taste.NONE);
 
 	// Attack-Increasing, Spice-Loving Natures:
 
 	/** Provides the spice-loving, attack increasing nature `Serious`. */
-	public static final PokemonNature ADAMANT = new PokemonNature().setName("Adamant").setStatIncreased(Stat.ATTACK)
-			.setStatDecreased(Stat.SPECIAL_ATTACK).setTasteFavorite(Taste.SPICY).setTasteDisliked(Taste.DRY);
+	public static final PokemonNature ADAMANT = new PokemonNature().setName("Adamant").setStatIncreased("Attack")
+			.setStatDecreased("Special Attack").setTasteFavorite(Taste.SPICY).setTasteDisliked(Taste.DRY);
 
 	/** Provides the spice-loving, attack increasing nature `Brave`. */
-	public static final PokemonNature BRAVE = new PokemonNature().setName("Brave").setStatIncreased(Stat.ATTACK)
-			.setStatDecreased(Stat.SPEED).setTasteFavorite(Taste.SPICY).setTasteDisliked(Taste.SWEET);
+	public static final PokemonNature BRAVE = new PokemonNature().setName("Brave").setStatIncreased("Attack")
+			.setStatDecreased("Speed").setTasteFavorite(Taste.SPICY).setTasteDisliked(Taste.SWEET);
 
 	/** Provides the spice-loving, attack increasing nature `Loneley`. */
-	public static final PokemonNature LONELY = new PokemonNature().setName("Lonely").setStatIncreased(Stat.ATTACK)
-			.setStatDecreased(Stat.DEFENSE).setTasteFavorite(Taste.SPICY).setTasteDisliked(Taste.SOUR);
+	public static final PokemonNature LONELY = new PokemonNature().setName("Lonely").setStatIncreased("Attack")
+			.setStatDecreased("Defense").setTasteFavorite(Taste.SPICY).setTasteDisliked(Taste.SOUR);
 
 	/** Provides the spice-loving, attack increasing nature `Naughty`. */
-	public static final PokemonNature NAUGHTY = new PokemonNature().setName("Naughty").setStatIncreased(Stat.ATTACK)
-			.setStatDecreased(Stat.SPECIAL_DEFENSE).setTasteFavorite(Taste.SPICY).setTasteDisliked(Taste.BITTER);
+	public static final PokemonNature NAUGHTY = new PokemonNature().setName("Naughty").setStatIncreased("Attack")
+			.setStatDecreased("Special Defense").setTasteFavorite(Taste.SPICY).setTasteDisliked(Taste.BITTER);
 
 	// Defense-Increasing, Sour-Loving Natures:
 
 	/** Provides the sour-loving, defense increasing nature `Bold`. */
-	public static final PokemonNature BOLD = new PokemonNature().setName("Bold").setStatIncreased(Stat.DEFENSE)
-			.setStatDecreased(Stat.ATTACK).setTasteFavorite(Taste.SOUR).setTasteDisliked(Taste.SPICY);
+	public static final PokemonNature BOLD = new PokemonNature().setName("Bold").setStatIncreased("Defense")
+			.setStatDecreased("Attack").setTasteFavorite(Taste.SOUR).setTasteDisliked(Taste.SPICY);
 
 	/** Provides the sour-loving, defense increasing nature `Relaxed`. */
-	public static final PokemonNature RELAXED = new PokemonNature().setName("Relaxed").setStatIncreased(Stat.DEFENSE)
-			.setStatDecreased(Stat.SPEED).setTasteFavorite(Taste.SOUR).setTasteDisliked(Taste.SWEET);
+	public static final PokemonNature RELAXED = new PokemonNature().setName("Relaxed").setStatIncreased("Defense")
+			.setStatDecreased("Speed").setTasteFavorite(Taste.SOUR).setTasteDisliked(Taste.SWEET);
 
 	/** Provides the sour-loving, defense increasing nature `Impish`. */
-	public static final PokemonNature IMPISH = new PokemonNature().setName("Impish").setStatIncreased(Stat.DEFENSE)
-			.setStatDecreased(Stat.SPECIAL_ATTACK).setTasteFavorite(Taste.SOUR).setTasteDisliked(Taste.DRY);
+	public static final PokemonNature IMPISH = new PokemonNature().setName("Impish").setStatIncreased("Defense")
+			.setStatDecreased("Special Attack").setTasteFavorite(Taste.SOUR).setTasteDisliked(Taste.DRY);
 
 	/** Provides the sour-loving, defense increasing nature `Lax`. */
-	public static final PokemonNature LAX = new PokemonNature().setName("Lax").setStatIncreased(Stat.DEFENSE)
-			.setStatDecreased(Stat.SPECIAL_DEFENSE).setTasteFavorite(Taste.SOUR).setTasteDisliked(Taste.BITTER);
+	public static final PokemonNature LAX = new PokemonNature().setName("Lax").setStatIncreased("Defense")
+			.setStatDecreased("Special Defense").setTasteFavorite(Taste.SOUR).setTasteDisliked(Taste.BITTER);
 
 	// Special Attack-Increasing, Dry-Loving Natures:
 
 	/** Provides the dry-loving, attack increasing nature `Modest`. */
-	public static final PokemonNature MODEST = new PokemonNature().setName("Modest").setStatIncreased(Stat.SPECIAL_ATTACK)
-			.setStatDecreased(Stat.ATTACK).setTasteFavorite(Taste.DRY).setTasteDisliked(Taste.SPICY);
+	public static final PokemonNature MODEST = new PokemonNature().setName("Modest").setStatIncreased("Special Attack")
+			.setStatDecreased("Attack").setTasteFavorite(Taste.DRY).setTasteDisliked(Taste.SPICY);
 
 	/** Provides the dry-loving, attack increasing nature `Mild`. */
-	public static final PokemonNature MILD = new PokemonNature().setName("Mild").setStatIncreased(Stat.SPECIAL_ATTACK)
-			.setStatDecreased(Stat.DEFENSE).setTasteFavorite(Taste.DRY).setTasteDisliked(Taste.SOUR);
+	public static final PokemonNature MILD = new PokemonNature().setName("Mild").setStatIncreased("Special Attack")
+			.setStatDecreased("Defense").setTasteFavorite(Taste.DRY).setTasteDisliked(Taste.SOUR);
 
 	/** Provides the dry-loving, attack increasing nature `Quiet`. */
-	public static final PokemonNature QUIET = new PokemonNature().setName("Quiet").setStatIncreased(Stat.SPECIAL_ATTACK)
-			.setStatDecreased(Stat.SPEED).setTasteFavorite(Taste.DRY).setTasteDisliked(Taste.SWEET);
+	public static final PokemonNature QUIET = new PokemonNature().setName("Quiet").setStatIncreased("Special Attack")
+			.setStatDecreased("Speed").setTasteFavorite(Taste.DRY).setTasteDisliked(Taste.SWEET);
 
 	/** Provides the dry-loving, attack increasing nature `Rash`. */
-	public static final PokemonNature RASH = new PokemonNature().setName("Rash").setStatIncreased(Stat.SPECIAL_ATTACK)
-			.setStatDecreased(Stat.SPECIAL_DEFENSE).setTasteFavorite(Taste.DRY).setTasteDisliked(Taste.BITTER);
+	public static final PokemonNature RASH = new PokemonNature().setName("Rash").setStatIncreased("Special Attack")
+			.setStatDecreased("Special Defense").setTasteFavorite(Taste.DRY).setTasteDisliked(Taste.BITTER);
 
 	// Special Defense-Increasing, Bitter-Loving Natures:
 
 	/** Provides the bitter-loving, defense increasing nature `Calm`. */
-	public static final PokemonNature CALM = new PokemonNature().setName("Calm").setStatIncreased(Stat.SPECIAL_DEFENSE)
-			.setStatDecreased(Stat.ATTACK).setTasteFavorite(Taste.BITTER).setTasteDisliked(Taste.SPICY);
+	public static final PokemonNature CALM = new PokemonNature().setName("Calm").setStatIncreased("Special Defense")
+			.setStatDecreased("Attack").setTasteFavorite(Taste.BITTER).setTasteDisliked(Taste.SPICY);
 
 	/** Provides the bitter-loving, defense increasing nature `Gentle`. */
-	public static final PokemonNature GENTLE = new PokemonNature().setName("Gentle").setStatIncreased(Stat.SPECIAL_DEFENSE)
-			.setStatDecreased(Stat.DEFENSE).setTasteFavorite(Taste.BITTER).setTasteDisliked(Taste.SOUR);
+	public static final PokemonNature GENTLE = new PokemonNature().setName("Gentle").setStatIncreased("Special Defense")
+			.setStatDecreased("Defense").setTasteFavorite(Taste.BITTER).setTasteDisliked(Taste.SOUR);
 
 	/** Provides the bitter-loving, defense increasing nature `Sassy`. */
-	public static final PokemonNature SASSY = new PokemonNature().setName("Sassy").setStatIncreased(Stat.SPECIAL_DEFENSE)
-			.setStatDecreased(Stat.SPEED).setTasteFavorite(Taste.BITTER).setTasteDisliked(Taste.SWEET);
+	public static final PokemonNature SASSY = new PokemonNature().setName("Sassy").setStatIncreased("Special Defense")
+			.setStatDecreased("Speed").setTasteFavorite(Taste.BITTER).setTasteDisliked(Taste.SWEET);
 
 	/** Provides the bitter-loving, defense increasing nature `Careful`. */
-	public static final PokemonNature CAREFUL = new PokemonNature().setName("Careful").setStatIncreased(Stat.SPECIAL_DEFENSE)
-			.setStatDecreased(Stat.SPECIAL_ATTACK).setTasteFavorite(Taste.BITTER).setTasteDisliked(Taste.DRY);
+	public static final PokemonNature CAREFUL = new PokemonNature().setName("Careful").setStatIncreased("Special Defense")
+			.setStatDecreased("Special Attack").setTasteFavorite(Taste.BITTER).setTasteDisliked(Taste.DRY);
 
 	// Speed-Increasing, Sweet-Loving Natures:
 
 	/** Provides the sweet-loving, speed increasing nature `Timid`. */
-	public static final PokemonNature TIMID = new PokemonNature().setName("Timid").setStatIncreased(Stat.SPEED)
-			.setStatDecreased(Stat.ATTACK).setTasteFavorite(Taste.SWEET).setTasteDisliked(Taste.SPICY);
+	public static final PokemonNature TIMID = new PokemonNature().setName("Timid").setStatIncreased("Speed")
+			.setStatDecreased("Attack").setTasteFavorite(Taste.SWEET).setTasteDisliked(Taste.SPICY);
 
 	/** Provides the sweet-loving, speed increasing nature `Hasty`. */
-	public static final PokemonNature HASTY = new PokemonNature().setName("Hasty").setStatIncreased(Stat.SPEED)
-			.setStatDecreased(Stat.DEFENSE).setTasteFavorite(Taste.SWEET).setTasteDisliked(Taste.SOUR);
+	public static final PokemonNature HASTY = new PokemonNature().setName("Hasty").setStatIncreased("Speed")
+			.setStatDecreased("Defense").setTasteFavorite(Taste.SWEET).setTasteDisliked(Taste.SOUR);
 
 	/** Provides the sweet-loving, speed increasing nature `Jolly`. */
-	public static final PokemonNature JOLLY = new PokemonNature().setName("Jolly").setStatIncreased(Stat.SPEED)
-			.setStatDecreased(Stat.SPECIAL_ATTACK).setTasteFavorite(Taste.SWEET).setTasteDisliked(Taste.DRY);
+	public static final PokemonNature JOLLY = new PokemonNature().setName("Jolly").setStatIncreased("Speed")
+			.setStatDecreased("Special Attack").setTasteFavorite(Taste.SWEET).setTasteDisliked(Taste.DRY);
 
 	/** Provides the sweet-loving, speed increasing nature `Naive`. */
-	public static final PokemonNature NAIVE = new PokemonNature().setName("Naive").setStatIncreased(Stat.SPEED)
-			.setStatDecreased(Stat.SPECIAL_DEFENSE).setTasteFavorite(Taste.SWEET).setTasteDisliked(Taste.BITTER);
+	public static final PokemonNature NAIVE = new PokemonNature().setName("Naive").setStatIncreased("Speed")
+			.setStatDecreased("Special Defense").setTasteFavorite(Taste.SWEET).setTasteDisliked(Taste.BITTER);
 
 	// Adds the natures listed above to a list for randomization purposes.
 	static {

0c860411cd1c56b455cdaaadef1c0ee2ffb18a76
Zach Taylor <zach.taylor673@gmail.com>
2014-04-24 18:00:00 -0500

    MovePropertyFactory added
    Following pattern of ItemAttributeFactory and AbilityEffectFactory

---
+++ src/main/java/org/jpokemon/api/MovePropertyFactory.java
@@ -0,0 +1,28 @@
+package org.jpokemon.api;
+
+/**
+ * Defines an algorithm for producing {@link Move#properties}. Provides a
+ * {@link Manager} reference for construction libraries to target. It is
+ * recommended for library code, and therefore, library consumers. A simple
+ * project may choose to completely ignore this class.
+ * 
+ * @author zach
+ * 
+ * @since 0.1
+ */
+public abstract class MovePropertyFactory {
+	/**
+	 * Indicates the manager used to register move property factories. May be
+	 * null.
+	 */
+	public static Manager<MovePropertyFactory> manager;
+
+	/**
+	 * Gets the unique name of this factory. Effectively, the class of move
+	 * properties that this factory produces
+	 */
+	public abstract String getName();
+
+	/** Builds an item attribute, given the options provided */
+	public abstract Object buildMoveProperty(String options);
+}

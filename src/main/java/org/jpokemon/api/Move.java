7db8695ceb5d9da538d5d8713385d444a8a7be80
Zach Taylor <zach.taylor673@gmail.com>
2014-04-24 16:54:39 -0500

    Move updated with things
    target is now a string to allow custom targetting definitions
    category added to define physical/special/status
    description added
    contestDescription added

--- src/main/java/org/jpokemon/api/Move.java
+++ src/main/java/org/jpokemon/api/Move.java
@@ -1,8 +1,7 @@
 package org.jpokemon.api;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Defines a move that can be learned and used by a Pokémon.
@@ -13,7 +12,7 @@ import java.util.HashMap;
  * {@link #properties}.
  * 
  * @author atheriel@gmail.com
- * @author Zach Taylor
+ * @author zach
  * 
  * @since 0.1
  */
@@ -21,132 +20,133 @@ public class Move {
 	/** Indicates the manager being used to register moves. May be ``null``. */
 	public static Manager<Move> manager;
 
-	/** Indicates attributed properties of the item. */
-	protected HashMap<String, String> properties = new HashMap<String, String>();
-
-	/** Indicates the name of this move. */
+	/** Indicates the name of this move */
 	protected String name;
 
-	/** Indicates the Type of this move. */
+	/** Indicates the type of this move */
 	protected String type;
 
-	/** Indicates the power of this move. */
+	/** Indicates the category of this move */
+	protected String category;
+
+	/** Indicates the description of this move */
+	protected String description;
+
+	/** Indicates the base PP for this move */
+	protected int pp;
+
+	/** Indicates the power of this move */
 	protected int power;
 
-	/** Indicates the accuracy of this move. */
-	protected int accuracy;
+	/** Indicates the accuracy of this move */
+	protected double accuracy;
 
-	/** Indicates the maximum PP for this move. */
-	protected int maxPP;
-
-	/** Indicates the priority of this move. */
-	protected int priority;
+	/** Indicates the targeting scheme of this move */
+	protected String target;
 
 	/** Indicates the contest category this move belongs to */
 	protected String contestCategory;
 
 	/** Indicates the contest appeal of this move */
-	protected int contestAppeal;
+	protected int appeal;
 
 	/** Indicates the contest jam of this move */
-	protected int contestJam;
+	protected int jam;
 
-	/** Indicates the contest appeal of this move */
-	protected int superContestAppeal;
+	/** Indicates the contest description of this move */
+	protected int contestDescription;
 
-	/** Indicates the contest jam of this move */
-	protected int superContestJam;
+	/** Indicates the priority of this move */
+	protected int priority;
 
-	/** Indicates whether this move uses `Special` stats. */
-	protected boolean special;
+	/** Indicates properties of this move */
+	protected List<Object> properties;
 
-	/** Indicates whether this move targets the user instead of the opponent. */
-	protected boolean selfTargeting;
-
-	/** Gets the name of this move. */
+	/** Gets the name of this move */
 	public String getName() {
 		return name;
 	}
 
-	/** Sets the name of this move. */
+	/** Sets the name of this move */
 	public Move setName(String name) {
 		this.name = name;
 		return this;
 	}
 
-	/** Gets the type of this move. */
+	/** Gets the type of this move */
 	public String getType() {
 		return type;
 	}
 
-	/** Sets the type of this move. */
+	/** Sets the type of this move */
 	public Move setType(String type) {
 		this.type = type;
 		return this;
 	}
 
-	/** Checks whether this move undergoes damage calculation. */
-	public boolean hasPower() {
-		return (power > 0) ? true : false;
+	/** Gets the category of this move */
+	public String getCategory() {
+		return category;
 	}
 
-	/** Gets the power of this move. */
+	/** Sets the category of this move */
+	public Move setCategory(String category) {
+		this.category = category;
+		return this;
+	}
+
+	/** Gets the description of this move */
+	public String getDescription() {
+		return description;
+	}
+
+	/** Sets the description of this move */
+	public Move setDescription(String description) {
+		this.description = description;
+		return this;
+	}
+
+	/** Gets the base PP for this move */
+	public int getPp() {
+		return pp;
+	}
+
+	/** Sets the base PP for this move */
+	public Move setPp(int pp) {
+		this.pp = pp;
+		return this;
+	}
+
+	/** Gets the power of this move */
 	public int getPower() {
 		return power;
 	}
 
-	/** Sets the power of this move. */
+	/** Sets the power of this move */
 	public Move setPower(int power) {
 		this.power = power;
 		return this;
 	}
 
-	/** Checks whether this move undergoes miss calculation. */
-	public boolean hasAccuracy() {
-		return (accuracy > 0) ? true : false;
-	}
-
-	/** Gets the accuracy of this move. */
-	public int getAccuracy() {
+	/** Gets the accuracy of this move */
+	public double getAccuracy() {
 		return accuracy;
 	}
 
-	/** Sets the accuracy of this move. */
-	public Move setAccuracy(int accuracy) {
+	/** Sets the accuracy of this move */
+	public Move setAccuracy(double accuracy) {
 		this.accuracy = accuracy;
 		return this;
 	}
 
-	/** Gets the maximum PP for this move. */
-	public int getMaxPP() {
-		return maxPP;
+	/** Gets the targeting scheme of this move */
+	public String getTarget() {
+		return target;
 	}
 
-	/** Sets the maximum PP for this move. */
-	public Move setMaxPP(int maxPP) {
-		this.maxPP = maxPP;
-		return this;
-	}
-
-	/** Check if this move uses the `Special` stats. */
-	public boolean isSpecial() {
-		return special;
-	}
-
-	/** Sets whether this move should use the `Special` stats. */
-	public Move setSpecial(boolean special) {
-		this.special = special;
-		return this;
-	}
-
-	/** Gets the move priority. */
-	public int getPriority() {
-		return priority;
-	}
-
-	/** Sets the move priority. */
-	public Move setPriority(int priority) {
-		this.priority = priority;
+	/** Sets the targeting scheme of this move */
+	public Move setTarget(String target) {
+		this.target = target;
 		return this;
 	}
 
@@ -162,110 +162,75 @@ public class Move {
 	}
 
 	/** Gets the contest appeal of this move */
-	public int getContestAppeal() {
-		return contestAppeal;
+	public int getAppeal() {
+		return appeal;
 	}
 
 	/** Sets the contest appeal of this move */
-	public Move setContestAppeal(int contestAppeal) {
-		this.contestAppeal = contestAppeal;
+	public Move setAppeal(int appeal) {
+		this.appeal = appeal;
 		return this;
 	}
 
 	/** Gets the contest jam of this move */
-	public int getContestJam() {
-		return contestJam;
+	public int getJam() {
+		return jam;
 	}
 
 	/** Sets the contest jam of this move */
-	public Move setContestJam(int contestjam) {
-		this.contestJam = contestjam;
+	public Move setJam(int jam) {
+		this.jam = jam;
 		return this;
 	}
 
-	/** Gets the super contest appeal of this move */
-	public int getSuperContestAppeal() {
-		return superContestAppeal;
+	/** Gets the contest description of this move */
+	public int getContestDescription() {
+		return contestDescription;
 	}
 
-	/** Sets the super contest appeal of this move */
-	public Move setSuperContestAppeal(int superContestAppeal) {
-		this.superContestAppeal = superContestAppeal;
+	/** Sets the contest description of this move */
+	public Move setContestDescription(int contestDescription) {
+		this.contestDescription = contestDescription;
 		return this;
 	}
 
-	/** Gets the super contest jam of this move */
-	public int getSuperContestJam() {
-		return superContestJam;
+	/** Gets the priority of this move */
+	public int getPriority() {
+		return priority;
 	}
 
-	/** Sets the super contest jam of this move */
-	public Move setSuperContestJam(int superContestJam) {
-		this.superContestJam = superContestJam;
+	/** Sets the priority of this move */
+	public Move setPriority(int priority) {
+		this.priority = priority;
 		return this;
 	}
 
-	/** Checks whether this move targets the user instead of its opponent. */
-	public boolean isSelfTargeting() {
-		return selfTargeting;
-	}
-
-	/** Sets whether this move should target the user instead of its opponent. */
-	public Move setSelfTargeting(boolean selfTargeting) {
-		this.selfTargeting = selfTargeting;
-		return this;
-	}
-
-	/** Checks if the move has a given property. */
-	public boolean hasProperty(String key) {
-		if (key == null) {
-			return false;
+	/** Adds a property to this move */
+	public Move addProperty(Object property) {
+		if (this.properties == null) {
+			this.properties = new ArrayList<Object>();
 		}
 
-		return properties.containsKey(key);
+		this.properties.add(property);
+		return this;
 	}
 
-	/** Sets a property of this move. It must have a unique name. */
-	public Move setProperty(String key, String value) {
-		if (!properties.containsKey(key)) {
-			properties.put(key, value);
+	/** Removes a property from this move */
+	public Move removeProperty(Object property) {
+		if (this.properties != null) {
+			this.properties.remove(property);
 		}
 
 		return this;
 	}
 
-	/**
-	 * Gets the property of this move for the given key
-	 * 
-	 * @param key The key of the property requested
-	 * 
-	 * @return The move's property under this name, or `null` if it does not
-	 *         possess one
-	 */
-	public String getProperty(String key) {
-		if (!properties.containsKey(key)) {
-			return null;
-		}
-
-		return properties.get(key);
-	}
-
-	/**
-	 * Gets all of the property keys of this Move as an UnmodifiableCollection
-	 * 
-	 * @return A collection of this Move's attribute keys
-	 */
-	public Collection<String> getAllProperties() {
-		return Collections.unmodifiableCollection(properties.keySet());
-	}
-
-	/** Gets the properties of this move */
-	public HashMap<String, String> getProperties() {
+	/** Gets all properties of this move */
+	public List<Object> getProperties() {
 		return properties;
 	}
 
-	/** Sets the properties of this move */
-	public Move setProperties(HashMap<String, String> properties) {
+	/** Sets all properties of this move */
+	public Move setProperties(List<Object> properties) {
 		this.properties = properties;
 		return this;
 	}

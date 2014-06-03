ccfef74f591d00cd9d633eea8c87445e39cb3dcf
Zach Taylor <zach.taylor673@gmail.com>
2014-06-03 01:02:22 -0500

    Pokedex update
    Fits the pattern of the rest of the api better now

--- src/main/java/org/jpokemon/api/Pokedex.java
+++ src/main/java/org/jpokemon/api/Pokedex.java
@@ -1,48 +1,92 @@
 package org.jpokemon.api;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- * A simple class for tracking which Pokemon have been seen or caught by a
+ * A simple class for tracking which Pokémon have been seen or caught by a
  * specific Player
  * 
  * @author zach
  */
 public class Pokedex {
-	/** Indicates the entries in this Pokedex by number */
-	protected String[] entries;
+	/** Indicates the entries in this Pokédex by number */
+	protected Map<String, Status> entries;
 
-	public int getCapacity() {
-		return entries.length;
+	/** Returns whether the specified Pokémon has been seen */
+	public boolean isSeen(String species) {
+		return isStatus(Status.SEEN, species);
 	}
 
-	public Pokedex setCapacity(int capacity) {
-		String[] newArray = new String[capacity];
+	/** Gets the list of Pokémon that have been seen */
+	public List<String> getSeen() {
+		return getStatus(Status.SEEN);
+	}
 
-		for (int i = 0; i < entries.length && i < capacity; i++) {
-			newArray[i] = entries[i];
+	/** Sets whether the specified Pokémon has been seen */
+	public Pokedex setSeen(String species) {
+		setStatus(Status.SEEN, species);
+		return this;
+	}
+
+	/** Returns whether the specified Pokémon has been caught */
+	public boolean isOwned(String species) {
+		return isStatus(Status.OWNED, species);
+	}
+
+	/** Gets the list of Pokémon that have been caught */
+	public List<String> getOwned() {
+		return getStatus(Status.OWNED);
+	}
+
+	/** Sets whether the specified Pokémon has been caught */
+	public Pokedex setOwned(String species) {
+		setStatus(Status.OWNED, species);
+		return this;
+	}
+
+	/** Removes the seen or owned status of a Pokémon from this Pokédex */
+	public Pokedex removeStatus(String species) {
+		if (entries != null) {
+			entries.remove(species);
 		}
 
-		entries = newArray;
 		return this;
 	}
 
-	public Pokedex putSeen(int number) {
-		if (!"owned".equals(entries[number])) {
-			entries[number] = "seen";
+	private boolean isStatus(Status status, String species) {
+		if (entries == null) {
+			return false;
 		}
 
-		return this;
+		return entries.get(species) == status;
 	}
 
-	public boolean isSeen(int number) {
-		return entries[number] != null;
+	private List<String> getStatus(Status status) {
+		List<String> species = new ArrayList<String>();
+
+		if (entries != null) {
+			for (Map.Entry<String, Status> statusEntry : entries.entrySet()) {
+				if (statusEntry.getValue() == status) {
+					species.add(statusEntry.getKey());
+				}
+			}
+		}
+
+		return species;
 	}
 
-	public Pokedex putOwned(int number) {
-		entries[number] = "owned";
-		return this;
+	private void setStatus(Status status, String species) {
+		if (entries == null) {
+			entries = new HashMap<String, Status>();
+		}
+
+		entries.put(species, status);
 	}
 
-	public boolean isOwned(int number) {
-		return "owned".equals(entries[number]);
+	protected enum Status {
+		SEEN, OWNED;
 	}
 }

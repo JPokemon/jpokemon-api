67920ca8fc56a399c264ccc3966efe07a09ddee2
Zach Taylor <zach.taylor673@gmail.com>
2014-06-24 17:51:31 -0500

    Added properties pattern to trainer class

--- src/main/java/org/jpokemon/api/TrainerClass.java
+++ src/main/java/org/jpokemon/api/TrainerClass.java
@@ -1,5 +1,8 @@
 package org.jpokemon.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Defines a class of trainers (such as Youngsters or Swimmers).
  * 
@@ -21,6 +24,9 @@ public class TrainerClass {
 	/** Indicates the description of the trainer class */
 	protected String description;
 
+	/** Indicates the properties of this trainer class */
+	protected Map<String, Object> properties;
+
 	/** Gets the name of this trainer class. */
 	public String getName() {
 		return this.name;
@@ -40,6 +46,60 @@ public class TrainerClass {
 	/** Sets the description of the trainer class */
 	public TrainerClass setDescription(String description) {
 		this.description = description;
+		return this;
+	}
+
+	/** Gets a property of this trainer class using class name as the key */
+	@SuppressWarnings("unchecked")
+	public <T> T getProperty(Class<T> clazz) {
+		return (T) getProperty(clazz.getName());
+	}
+
+	/** Gets a property of this trainer class by name */
+	public Object getProperty(String name) {
+		if (properties == null) {
+			return null;
+		}
+
+		return properties.get(name);
+	}
+
+	/** Adds a property to this trainer class using the class name as the key */
+	public TrainerClass addProperty(Object object) {
+		return setProperty(object.getClass().getName(), object);
+	}
+
+	/** Sets a property of this trainer class */
+	public TrainerClass setProperty(String name, Object property) {
+		if (properties == null) {
+			properties = new HashMap<String, Object>();
+		}
+
+		properties.put(name, property);
+		return this;
+	}
+
+	/** Removes a property from this trainer class by name */
+	public TrainerClass removeProperty(String name) {
+		if (properties != null) {
+			properties.remove(name);
+		}
+
+		return this;
+	}
+
+	/** Gets all properties of this trainer class */
+	public Map<String, Object> getProperties() {
+		if (properties == null) {
+			properties = new HashMap<String, Object>();
+		}
+
+		return properties;
+	}
+
+	/** Sets all properties of this trainer class */
+	public TrainerClass setProperties(Map<String, Object> properties) {
+		this.properties = properties;
 		return this;
 	}
 }
